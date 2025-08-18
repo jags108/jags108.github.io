0a765d49aadc3c026540a2e89481a994a74216e8
Jags <pvbahekar@gmail.com>
2025-08-18 23:38:56 +0530

    Add files via upload

---
+++ assets/js/product.js
@@ -0,0 +1,142 @@
+// ! INITIALIZATION
+var activeColor = null;
+
+// ! GET PRODUCT NAME
+const params = new URLSearchParams(window.location.search);
+const product_name = params.get("name");
+
+// ! CHAGNE APPEARANCE OF HEADER ON SCROLL
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.pageYOffset;
+  const header = document.querySelector("header");
+  if (scrollPosition > 40) {
+    header.classList.add("active");
+  } else {
+    if (header.classList.contains("active")) {
+      header.classList.remove("active");
+    }
+  }
+});
+
+//? IMAGE FUNCTIONALITY COMPLETE CODE ?//
+
+function setMainImage(newActiveSideImage) {
+  const oldActiveSideImage = document.querySelector(`.all-images .active`);
+  if (oldActiveSideImage) {
+    oldActiveSideImage.classList.remove("active");
+  }
+
+  const mainImage = document.querySelector(`.onscreen-image img`);
+  mainImage.src = newActiveSideImage.src;
+  newActiveSideImage.classList.add("active");
+}
+
+function generateSideImages(images) {
+  const allImages = document.querySelector(`.all-images`);
+  allImages.innerHTML = "";
+  imageIndex = 0;
+  firstImage = null;
+  images.forEach((src) => {
+    const img = document.createElement("img");
+    img.src = src;
+    img.dataset.colorId = imageIndex + 1;
+    img.addEventListener("click", () => {
+      setMainImage(img);
+    });
+    allImages.appendChild(img);
+    if (imageIndex === 0) {
+      firstImage = img;
+    }
+    imageIndex++;
+  });
+  setMainImage(firstImage);
+}
+
+//? COLOR FUNCTIONALITY COMPLETE CODE ?//
+
+// ! COLOR CLICK FUNCTION
+function clickColor(colorId) {
+  const currentActive = document.querySelector(`.product-colors .active`);
+  if (currentActive) {
+    currentActive.classList.remove("active");
+  }
+  activeColor = colorId;
+  const newActive = document.querySelector(
+    `.product-colors [data-color-id="${colorId}"]`
+  );
+  setMainImage(
+    document.querySelector(`.all-images [data-color-id="${colorId}"]`)
+  );
+  if (newActive) {
+    newActive.classList.add("active");
+  }
+}
+
+// ! SET VALUES OF THE PAGE
+function setValues(product) {
+  document.querySelector(".title").textContent = product.name;
+  document.querySelector(".price").textContent = `₹ ${product.price}/- `;
+
+  let isOutOfStock = true;
+
+  if (product.colors.length > 1) {
+    product.colors.forEach((color) => {
+      const div = document.createElement("div");
+      div.classList.add("square");
+      div.dataset.colorId = color.id;
+
+      // Add Image or Named Color
+      if (availableColors.includes(color.name)) {
+        div.classList.add(`product-${color.name}`);
+      } else {
+        const image = document.createElement("img");
+        image.src = color.name;
+        div.appendChild(image);
+      }
+
+      // Button Functionality
+      if (color.stock !== 0) {
+        if (activeColor === null) {
+          activeColor = color.id;
+        }
+        div.addEventListener("click", () => {
+          clickColor(color.id);
+        });
+        isOutOfStock = false;
+      } else {
+        div.classList.add("out-of-stock");
+      }
+
+      document.querySelector(".product-colors").appendChild(div);
+    });
+
+    // Set Active Item
+    if (activeColor !== null) {
+      const firstActive = document.querySelector(
+        `.product-colors [data-color-id="${activeColor}"]`
+      );
+      if (firstActive) {
+        firstActive.classList.add("active");
+      }
+    }
+  } else {
+    document.querySelector(".color-details-conatainer").style.display = "none";
+    if (product.colors[0].stock !== 0) {
+      isOutOfStock = false;
+    }
+  }
+
+  if (isOutOfStock) {
+    document.querySelector(".buy-btn").classList.add("disabled");
+    document.querySelector(".buy-btn a").textContent = "Out Of Stock";
+  }
+}
+
+// ! FETCH PRODUCTS AND FIND MATCH
+fetch("/assets/data/products.json")
+  .then((res) => res.json())
+  .then((products) => {
+    const product = products.find((p) => p.name === product_name);
+    setValues(product);
+    generateSideImages(product.images);
+  });
---
+++ assets/js/script.js
@@ -0,0 +1,124 @@
+const container = document.querySelector(".products-container");
+products = [];
+let currentIndex = 0;
+const chunkSize = 6;
+
+// ! CHAGNE APPEARANCE OF HEADER ON SCROLL
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.pageYOffset;
+  const header = document.querySelector("header");
+  if (scrollPosition > 350) {
+    header.classList.add("active");
+  } else {
+    if (header.classList.contains("active")) {
+      header.classList.remove("active");
+    }
+  }
+  if (scrollPosition > 500) {
+    header.classList.add("solid");
+  } else {
+    if (header.classList.contains("solid")) {
+      header.classList.remove("solid");
+    }
+  }
+});
+
+function getColorsHTML(product) {
+  let colorsHTML = "";
+  let isOutOfStock = true;
+  if (product.colors && Array.isArray(product.colors)) {
+    product.colors.forEach((color) => {
+      // Create a div for each color
+      let divHTML = `<div class="`;
+      if (color.stock === 0) {
+        divHTML += "out-of-stock ";
+      } else {
+        isOutOfStock = false;
+      }
+      if (availableColors.includes(color.name)) {
+        divHTML += `product-${color.name}`;
+      }
+      divHTML += '" ';
+
+      divHTML += ">";
+
+      // If it's a named color, use a class
+      if (!availableColors.includes(color.name)) {
+        divHTML += `<img src="${color.name}" alt="color image" />`;
+      }
+
+      divHTML += "</div>";
+      colorsHTML += divHTML;
+    });
+  }
+  return [colorsHTML, isOutOfStock];
+}
+
+function generateProductCards(dataChunk, append = false) {
+  if (!append) container.innerHTML = "";
+
+  dataChunk.forEach((product) => {
+    const [colorsHTML, isOutOfStock] = getColorsHTML(product);
+    let styleOfContainer = "";
+    let isOutOfStockText = "";
+    if (product.colors.length > 1) styleOfContainer += 'style="opacity:1"';
+    if (isOutOfStock) isOutOfStockText = "out-of-stock";
+
+    const card = document.createElement("div");
+    card.className = "product";
+    card.innerHTML = `
+        <div class="product-image">
+    <img src="${product.image}" />
+    <div class="product-colors" ${styleOfContainer}>
+      ${colorsHTML}
+    </div>
+  </div>
+  <div class="product-content">
+    <div class="product-info">
+      <h6>${product.name}</h6>
+      <p>₹ ${product.price}</p>
+    </div>
+    <a><i class="ri-shopping-cart-line ${isOutOfStockText}"></i></a>
+  </div>
+    `;
+
+    container.appendChild(card);
+  });
+
+  const productCards = document.querySelectorAll(".product");
+  productCards.forEach((productCard, idx) => {
+    productCard.addEventListener("click", () => {
+      const product = products[currentIndex - chunkSize + idx];
+      window.location.href = `/product.html?name=${encodeURIComponent(
+        product.name
+      )}`;
+    });
+  });
+}
+
+function loadNextChunk() {
+  const loadMoreBtn = document.querySelector(".load-more-btn");
+  const nextChunk = products.slice(currentIndex, currentIndex + chunkSize);
+  generateProductCards(nextChunk, true);
+  currentIndex += chunkSize;
+
+  if (currentIndex >= products.length) {
+    loadMoreBtn.style.display = "none";
+  }
+}
+
+fetch("./assets/data/products.json")
+  .then((res) => res.json())
+  .then((data) => {
+    products = data;
+    loadNextChunk();
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadMoreBtn = document.querySelector(".load-more-btn");
+  if (loadMoreBtn) {
+    loadMoreBtn.addEventListener("click", () => {
+      loadNextChunk();
+    });
+  }
+});
